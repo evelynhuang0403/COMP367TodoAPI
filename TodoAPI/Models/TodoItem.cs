f6c320e0f86598b20e08c7842ce1e70e1152d578
Evelyn Huang <evelynhuang1116@gmail.com>
2026-02-05 10:10:02 -0500

    Add quiz#2 (closes #1)

--- TodoAPI/Models/TodoItem.cs
+++ TodoAPI/Models/TodoItem.cs
@@ -4,6 +4,7 @@
     {
         public long Id { get; set; }
         public string? Name { get; set; }
-        public bool IsComplete { get; set; }
+        public bool IsComplete { get; set; } 
+        // Update C# Codes for quiz 2
     }
 }
